0c174d921e4fdff756f6e0c49f73b0cbb73252f9
Max Hirschhorn <maxh@caltech.edu>
2012-12-30 18:48:29 -0800

    Created Centipede interface.
    Serves as the crawler.

---
+++ src/edu/caltech/visemet/wanderer/Centipede.java
@@ -0,0 +1,63 @@
+package edu.caltech.visemet.wanderer;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ *
+ * @param <R> the type of resources handled by the centipede
+ *
+ * @author Max Hirschhorn #visemet
+ */
+public interface Centipede<R extends ResourceWrapper> {
+
+    /**
+     * Initializes this centipede.
+     */
+    void initialize();
+
+    /**
+     * Adds the specified resource to the queue of resources that are examined.
+     *
+     * @param resource the resource to examine
+     *
+     * @return
+     */
+    boolean examine(R resource);
+
+    /**
+     * Returns the list of body extractors used by this centipede.
+     *
+     * @return the list of body extractors used by this centipede
+     */
+    List<BodyExtractor<R>> retrieveBodyExtractors();
+
+    /**
+     * Adds the specified body extractor to the list of body extractors used by
+     * this centipede.
+     *
+     * @param bodyExtractor the body extractor to apply
+     */
+    void apply(BodyExtractor<R> bodyExtractor);
+
+    /**
+     * Executes this centipede.
+     *
+     * @throws InterruptedException if an interrupted exception occurs
+     * @throws IOException if an input or output exception occurs
+     */
+    void execute() throws InterruptedException, IOException;
+
+    /**
+     * Suspends this centipede.
+     */
+    void suspend();
+
+    /**
+     * Returns whether or not this centipede should continue to execute.
+     *
+     * @return {@code true} if this centipede should terminate, and
+     * {@code false} otherwise
+     */
+    boolean shouldTerminate();
+}
