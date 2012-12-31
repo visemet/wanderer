355df7624f9ebb9ac2d795a97bf867bb510078e6
Max Hirschhorn <maxh@caltech.edu>
2012-12-30 17:39:13 -0800

    Created ResourceWrapper interface.
    Wraps a URI.

---
+++ src/edu/caltech/visemet/wanderer/ResourceWrapper.java
@@ -0,0 +1,15 @@
+package edu.caltech.visemet.wanderer;
+
+/**
+ *
+ * @author Max Hirschhorn #visemet
+ */
+public interface ResourceWrapper {
+
+    /**
+     * Returns the resource wrapped by this instance.
+     *
+     * @return a URI
+     */
+    String getResource();
+}
