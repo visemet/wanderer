84da8284c8828c11c21d20e763bd8c41bb222381
Max Hirschhorn <maxh@caltech.edu>
2013-01-01 11:37:34 -0800

    Created Resources class.
    Handles normalization of URIs.

---
+++ src/edu/caltech/visemet/wanderer/Resources.java
@@ -0,0 +1,40 @@
+package edu.caltech.visemet.wanderer;
+
+/**
+ *
+ * @author Max Hirschhorn #visemet
+ */
+public class Resources {
+
+    /**
+     * Class constructor.
+     */
+    private Resources() { }
+
+    /**
+     * Normalizes the specified path with respect to the specified base
+     * resource.
+     *
+     * @param baseResource the base resource
+     * @param path the path
+     *
+     * @return a URI
+     */
+    public static String normalize(String baseResource, String path) {
+        String URI = null;
+
+        if (path.startsWith("http://") || path.startsWith("https://")) {
+            URI = path;
+        } else if (path.startsWith("/")) {
+            int index = baseResource.indexOf("/", 7);
+
+            if (index != -1) {
+                URI = baseResource.substring(0, index).concat(path);
+            } else {
+                URI = baseResource.concat(path);
+            }
+        }
+
+        return URI;
+    }
+}
