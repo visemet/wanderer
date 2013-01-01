dd639aaae3235d463dd0e853900ea7ea4efd560a
Max Hirschhorn <maxh@caltech.edu>
2013-01-01 13:55:01 -0800

    Created ResourceSieve interface.

---
+++ src/edu/caltech/visemet/wanderer/ResourceSieve.java
@@ -0,0 +1,20 @@
+package edu.caltech.visemet.wanderer;
+
+/**
+ *
+ * @param <R> the type of resources processed by the sieve
+ *
+ * @author Max Hirschhorn #visemet
+ */
+public interface ResourceSieve<R extends ResourceWrapper> {
+
+    /**
+     * Return whether or not the specified resource should be examined.
+     *
+     * @param resource the resource to potentially examine
+     *
+     * @return {@code true} if this resource should be included, and
+     * {@code false} otherwise
+     */
+    boolean shouldInclude(R resource);
+}
